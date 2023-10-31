30bff4cf5a614af3bf0d8ce4ee9124dcfa767a22
Andrew Nikolic <a.nikolic@outlook.com>
2023-10-31 17:21:50 +1100

    Initial commit

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY=""
-SLACK_BOT_TOKEN=""
-SLACK_APP_TOKEN=""
-SLACK_SIGNING_SECRET=""
+OPENAI_API_KEY="enter OpenAI secret key here"
+SLACK_BOT_TOKEN="enter bot token from Slack here"
+SLACK_APP_TOKEN="enter Socket Mode app token from Slack here"
+SLACK_SIGNING_SECRET="enter signing secret from Slack here"
